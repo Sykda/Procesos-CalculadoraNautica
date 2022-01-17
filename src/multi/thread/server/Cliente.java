05d4349d56b708059f4c34c24a702a3048f0454d
Sykda <93369567+Sykda@users.noreply.github.com>
2022-01-17 14:23:44 +0100

    commit

---
+++ src/multi/thread/server/Cliente.java
@@ -0,0 +1,43 @@
+package multi.thread.server;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+
+	
+	public static void main(String[] args) throws Exception {
+
+		String resultado;
+
+		System.out.println("CLIENT: ");
+
+		Socket clientSocket = new Socket("127.0.0.1", 2009);
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduce una operación: ");
+		String suma = sc.nextLine();
+		
+		// Create an output stream attached to the socket
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+		// Create an input stream attached to socket
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+		// Send a line to server
+		outToServer.writeBytes(suma + '\n');
+
+		// Read a line from server
+		resultado = inFromServer.readLine();
+
+		System.out.println("FROM SERVER: " + resultado);
+
+		clientSocket.close();
+		sc.close();
+
+	}
+}
---
+++ src/multi/thread/server/Hilo.java
@@ -0,0 +1,72 @@
+package multi.thread.server;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+public class Hilo implements Runnable {
+
+	String clientSentence;
+	Socket connectionSocket;
+
+	public Hilo(Socket connectionSocket) {	
+		this.connectionSocket = connectionSocket;
+		
+	}
+
+	public void run() {
+		
+		while(true) {
+
+			try {
+				// Create input stream, attached to socket
+				BufferedReader inFromClient = new BufferedReader(
+						new InputStreamReader(connectionSocket.getInputStream()));
+
+				// Create output stream, attached to socket
+				DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+				// Read in line from socket
+				clientSentence = inFromClient.readLine();
+				
+				StringTokenizer st = new StringTokenizer(clientSentence);
+				int x= Integer.parseInt(st.nextToken());
+				String operacion= st.nextToken();
+				int y= Integer.parseInt(st.nextToken());
+				int resultado = 0;
+		
+				if (clientSentence.contains("+")) {
+					resultado= x+y;
+				}
+				else if (clientSentence.contains("-")) {
+					resultado= x-y;
+				}
+				
+				else if (clientSentence.contains("*")) {
+					resultado= x*y;
+				}
+				else if (clientSentence.contains("/")) {
+					resultado= x/y;
+				}				
+
+				String modifiedSentence=String.valueOf(resultado);				
+				outToClient.writeBytes(modifiedSentence);
+
+				System.out.println("CLIENT Conectado desde: " + connectionSocket.getInetAddress().getHostAddress() + ":"
+						+ connectionSocket.getPort());
+				System.out.println("SERVER Client sentence: " + clientSentence);
+				System.out.println("SERVER Modified sentence: " + resultado);
+				
+				
+			} catch (Exception ex) {
+				System.out.println("Error: "+ ex.getMessage());
+			}
+			
+			
+		}
+		
+	}
+
+}
